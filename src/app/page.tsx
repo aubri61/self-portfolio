e1c2e66cf5170e12800f190f1fc30b2302ef7079
aubri61 <mingbee611@yonsei.ac.kr>
2025-05-14 16:46:29 +0900

    [feat] about done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import About from "@/components/About";
 
 export default function Home() {
   return (
-    <main className="flex justify-center items-center flex-col">
+    <main className="flex justify-center items-center flex-col px-5">
       <About />
     </main>
   );
